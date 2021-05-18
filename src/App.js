26807c437021fa6d989fb9d08f5a7f252bb21250
Pongsak Pattamasaevi <ppattamasaevi@gmail.com>
2021-05-18 17:39:41 +0800

    Refactored App.js into a Hook component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
+const App = () => {
+  return (
+    <>
+      <h1>
+        Hello MVP
+      </h1>
+    </>
+  );
 }
+
 
 export default App;
